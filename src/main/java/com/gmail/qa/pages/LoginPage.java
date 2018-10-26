7b36ecf18398aec7f4a891c736811eddbdd4eba1
Abhishek Sharma <abhishek@om.omnieserver.com>
2018-10-26 16:11:39 +0530

    First Commit

---
+++ src/main/java/com/gmail/qa/pages/LoginPage.java
@@ -0,0 +1,56 @@
+package com.gmail.qa.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.gmail.qa.base.TestBase;
+
+public class LoginPage extends TestBase{
+	
+	//Page factory = OR:
+	@FindBy(id="identifierId")
+	WebElement Email;
+	
+	@FindBy(id="identifierNext")
+	WebElement nextBtn;
+	
+	
+	@FindBy(xpath="//input[@name='password']")
+	WebElement password;
+	
+	@FindBy(id="passwordNext")
+	WebElement nextBtn1;
+	
+	@FindBy(xpath="//span[class(text(),'Create account')]")
+	WebElement createbtn;
+	
+	@FindBy(id="logo")
+	WebElement googlelogo;
+
+	//Initializing the Page objects/Factory/OR:
+	public LoginPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	//Action:
+	public String validateLoginPageTitle() {
+		return driver.getTitle();
+	}
+	
+	public boolean validateGoogleImage() {
+		return googlelogo.isDisplayed();
+	}
+	
+		public HomePage login(String un,String pwd) {
+			Email.sendKeys(un);
+			nextBtn.click();
+			password.sendKeys(pwd);
+			nextBtn1.click();
+			return new HomePage();
+			
+		}
+
+	}
+
+
---
+++ src/test/java/com/gmail/qa/testcases/LoginPageTest.java
@@ -0,0 +1,61 @@
+package com.gmail.qa.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.gmail.qa.base.TestBase;
+import com.gmail.qa.pages.HomePage;
+import com.gmail.qa.pages.LoginPage;
+
+public class LoginPageTest extends TestBase{
+	LoginPage loginPage;
+	HomePage homePage;
+	
+	public LoginPageTest() {
+		super();
+		
+	}
+	
+	@BeforeMethod
+	public void setUp() {
+		initialization();
+		 loginPage = new LoginPage();  //Create LoginPage class object:
+	}
+	
+	@Test(priority=1)
+	public void loginPageTitleTest() {
+		String title = loginPage.validateLoginPageTitle();
+		Assert.assertEquals(title, "Gmail");
+	}
+	
+	@Test(priority=2)
+	public void googleLogoImageTest()
+	{
+		boolean flag = loginPage.validateGoogleImage();
+		Assert.assertTrue(flag);
+	}
+	
+	@Test(priority=3)
+	public void loginTest() {
+		homePage = loginPage.login(prop.getProperty("Email"), prop.getProperty("password"));
+	}
+	
+	
+	
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
